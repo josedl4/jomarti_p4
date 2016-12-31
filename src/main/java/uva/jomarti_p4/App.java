c66bfeb5bbbcb201aad5a1d632c4e42245e8b582
Jose Luis Martín Martín <joseop21@gmail.com>
2016-12-31 21:36:37 +0100

    Initial commit

---
+++ src/main/java/uva/jomarti_p4/App.java
@@ -0,0 +1,13 @@
+package uva.jomarti_p4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

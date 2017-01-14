c8e13b1c4ab081e26073bc8532ee1817be1d6e2a
Jose Luis Martín Martín <joseop21@gmail.com>
2017-01-14 16:03:34 +0100

    Added javadoc in asignatura class and made black box test of the asignatura constructor

--- src/main/java/uva/jomarti_p4/Asignatura.java
+++ src/main/java/uva/jomarti_p4/Asignatura.java
@@ -17,13 +17,22 @@ public class Asignatura {
 	
 	
 	/**
-	 * Constructor de la clase asignatura con los valores necesarios para la creación
+	 * Constructor de la clase asignatura con los valores necesarios para la creación.
 	 * 
-	 * @param nombre	Nombre de la asignatura
-	 * @param descripcion	Descripción de la asignatura
-	 * @param calificacionMax	Calificación maxima de la asignatura
-	 * @param fechaInicio	Fecha de Inicio de la asignatura
-	 * @param fechaFin		Fehca de Fin de la asignatura
+	 * @param nombre	Nombre de la asignatura.
+	 * @param descripcion	Descripción de la asignatura.
+	 * @param calificacionMax	Calificación maxima de la asignatura.
+	 * @param fechaInicio	Fecha de Inicio de la asignatura.
+	 * @param fechaFin		Fehca de Fin de la asignatura.
+	 * 
+	 * @throws IllegalArgumentException Si el nombre tiene valor null.
+	 * @throws IllegalArgumentException Si la descripcion tiene valor null.
+	 * @throws IllegalArgumentException Si la nota tiene un valor =< que 0,
+	 * solo valores estrictamentes mayores que 0.
+	 * @throws IllegalArgumentException Si la fecha de inicio tiene valor null.
+	 * @throws IllegalArgumentException Si la fecha de fin tiene valor null.
+	 * @throws IllegalArgumentException Si la fecha de fin es anterior a la
+	 * de inicio tiene valor null.
 	 */
 	public Asignatura(String nombre, String descripcion, double calificacionMax,
 			GregorianCalendar fechaInicio, GregorianCalendar fechaFin) {
@@ -31,74 +40,149 @@ public class Asignatura {
 	}
 
 
+	/**
+	 * Get que devuelve el nombre de la asignatura.
+	 * 
+	 * @return nombre Nombre de la signatura.
+	 */
 	public String getNombre() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 
+	/**
+	 * Get que devuelve la descricion de la asignatura.
+	 * 
+	 * @return descripcion Descripcion de la asignatura.
+	 */
 	public String getDescripcion() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 
+	/**
+	 * Get que devuelve la calificacion maxima de la asignatura.
+	 * 
+	 * @return notaMax Nota maxima de la asignatura.
+	 */
 	public double getCalificacionMax() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-
+	
+	/**
+	 * Get que devuelve la fecha de inicio de la asignatura.
+	 * 
+	 * @return fechaInicio Fecha de inicio de la asignatura.
+	 */
 	public GregorianCalendar getFechaInicio() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 
+	/**
+	 * Get que devuelve la fecha de fin de la asignatura.
+	 * 
+	 * @return fechaFin Fecha de fin de la asignatura.
+	 */
 	public GregorianCalendar getFechaFin() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 
+	/**
+	 * Devulve true en caso de estar vacia (no tener ninguna prueba) o
+	 * false en caso contrario (tener pruebas añadidas).
+	 * 
+	 * @return result Estado de estar vacia.
+	 */
 	public boolean esVacia() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 
+	/**
+	 * Devuelve la cantidad de pruebas asociadas a la asignatura.
+	 * 
+	 * @return result Cantidad pruebas asociadas.
+	 */
 	public int cantidadPruebas() {
 		// TODO Auto-generated method stub
 		return -1;
 	}
 
 
-	public void addPrueba(Prueba pruebaAux) {
+	/**
+	 * Añade una nueva prueba mas a la asignatura. 
+	 * 
+	 * @param prueba Nueva prueba asociada a la asignatura.
+	 * 
+	 * @throws IllegalArgumentException Si la prueba pasada como argumento tiene valor null.
+	 * @throws IllegalArgumentException Si la prueba pasada como argumento ya esta asociada.
+	 * @throws IllegalArgumentException Si la prueba pasada como argumento excede la suma de
+	 * todos los pesos de las distintas pruebas. 
+	 */
+	public void addPrueba(Prueba prueba) {
 		// TODO Auto-generated method stub	
 	}
 
 
+	/**
+	 * Devuelve el listado de las pruebas asociadas.
+	 * 
+	 * @return listadoPruebas Listado de las pruebas asociadas a la asignatura.
+	 */
 	public ArrayList<Prueba> getListadoPruebas() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 
+	/**
+	 * Devuelve el peso restante que nos queda por añadir para poder completar las
+	 * pruebas asociadas a nuestras asignaturas.
+	 * 
+	 * @return pesoRestante Peso restante para alcanzar el valor 1 entre las disitintas pruebas.
+	 */
 	public double pesoRestante() {
 		// TODO Auto-generated method stub
 		return -1.0;
 	}
 
 
+	/**
+	 * Genera un listado de las calificaciones parciles de todos los alumnos pertenecientes
+	 * a la asignatura con su calificación actual, mediante las pruebas que se le han evaluado.
+	 * 
+	 * @return calificacionesParciales Listado de las calificaciones parciales en el momento que
+	 * se llama a la función.
+	 */
 	public Hashtable<String, Double> calificacionesParciales() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 
+	/**
+	 * Genera un listado de las calificaciones finales de todos los alumnos pertenecientes
+	 * a la asignatura con su calificación final en la asignatura, mediante el conjunto de 
+	 * todas las pruebas. Unicamente puede ser usado cuando el conjunto de pruebas sume el
+	 * peso 1 en la asignatura y todas se hayan calificado por completo.
+	 * 
+	 * @return calificacionesFinales Listado de las calificaciones finales de todos los alumnos
+	 * pertenecientes a dicha asignatura. 
+	 * 
+	 * @throws IllegalStateException Si el conjunto de pruebas no suma un peso de valor 1.
+	 * @throws IllegalStateException Si no todas las asignaturas se han calificado por completo.
+	 */
 	public Hashtable<String, Double> calificacionesFinales() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
--- src/test/java/uva/jomarti_p4/AsignaturaBlackBoxTest.java
+++ src/test/java/uva/jomarti_p4/AsignaturaBlackBoxTest.java
@@ -2,13 +2,88 @@ package uva.jomarti_p4;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class AsignaturaBlackBoxTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private Asignatura asignaturaAux;
+	private Prueba pruebaAux1, pruebaAux2;
+
+	@Before
+	public void setupAsignatura() {
+		asignaturaAux = new Asignatura("Nombre",
+				"Descripcion sobre la asignatura", 10.0,
+				new GregorianCalendar(2016, 12, 31), 
+				new GregorianCalendar(2017, 12, 31));
+	}
+	
+	@Before
+	public void setupPrueba() {
+		pruebaAux1 = new Prueba(new Date(), "Test 1", "Test del tema 1...", 10, 0.1);
+		pruebaAux2 = new Prueba(new Date(), "Examen Final", "Examen final de la asignatura",
+				10, 1);
+		
+		pruebaAux1.calificar("id001", 5, new Date());
+		pruebaAux1.calificar("id002", 9.5, new Date());
+		
+		pruebaAux2.calificar("id001", 5, new Date());
+		pruebaAux2.calificar("id002", 9.5, new Date());
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testAsignaturaNombreNull() {
+		Asignatura asignatura = new Asignatura(null,
+				"Descripcion sobre la asignatura", 10.0,
+				new GregorianCalendar(2016, 12, 31), 
+				new GregorianCalendar(2017, 12, 31));
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testAsignaturaDecripcionNull() {
+		Asignatura asignatura = new Asignatura("Nombre",
+				null, 10.0,
+				new GregorianCalendar(2016, 12, 31), 
+				new GregorianCalendar(2017, 12, 31));
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testAsignaturaNotaMaxErronea() {
+		Asignatura asignatura = new Asignatura("Nombre",
+				"Descripcion sobre la asignatura", 0.0,
+				new GregorianCalendar(2016, 12, 31), 
+				new GregorianCalendar(2017, 12, 31));
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testAsignaturaFechaInicioNull() {
+		Asignatura asignatura = new Asignatura("Nombre",
+				"Descripcion sobre la asignatura", 10.0,
+				null, new GregorianCalendar(2017, 12, 31));
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testAsignaturaFechaFinNull() {
+		Asignatura asignatura = new Asignatura("Nombre",
+				"Descripcion sobre la asignatura", 10.0,
+				new GregorianCalendar(2016, 12, 31), null);
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testAsignaturaEstadoFechasIncorrecto() {
+		Asignatura asignatura = new Asignatura("Nombre",
+				"Descripcion sobre la asignatura", 10.0,
+				new GregorianCalendar(2016, 12, 31),
+				new GregorianCalendar(2015, 12, 31));
 	}
 
 }
